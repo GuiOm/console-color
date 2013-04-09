a91b45becd00b1a56cba0bdcb797ab9e8e8c31bb
Guillaume Cheron <guillaum.cheron@gmail.com>
2013-04-09 22:53:13 +0200

    Some code refactoring

--- consoleColor.py
+++ consoleColor.py
@@ -2,35 +2,29 @@
 # -*- coding: utf-8 -*-
 
 class Color():
-	reset = "\033[0m"
-	styles = {
-		"bold"       :    "\033[1m",
-		"underline"  :    "\033[4m",
-		"reverse"    :    "\033[7m"}
-	colors = {
-		"black"      :    "\033[30m", 
-		"red"        :    "\033[31m",
-		"green"      :    "\033[32m",
-		"yellow"     :    "\033[33m",
-		"blue"       :    "\033[34m",
-		"magenta"    :    "\033[35m",
-		"cyan"       :    "\033[36m",
-		"white"      :    "\033[37m"}
-	backgroundColors = {
-		"on_black"   :    "\033[40m", 
-		"on_red"     :    "\033[41m",
-		"on_green"   :    "\033[42m",
-		"on_yellow"  :    "\033[43m",
-		"on_blue"    :    "\033[44m",
-		"on_magenta" :    "\033[45m",
-		"on_cyan"    :    "\033[46m",
-		"on_white"   :    "\033[47m"}
+	RESET = "\033[0m"
+	BOLD = "\033[1m"
+	UNDERLINE = "\033[4m"
+	REVERSE = "\033[7m"
+	BLACK = "\033[30m"
+	RED = "\033[31m"
+	GREEN = "\033[32m"
+	YELLOW = "\033[33m"
+	BLUE = "\033[34m"
+	MAGENTA = "\033[35m"
+	CYAN = "\033[36m"
+	WHITE = "\033[37m"
+	ON_BLACK = "\033[40m"
+	ON_RED = "\033[41m"
+	ON_GREEN = "\033[42m"
+	ON_YELLOW = "\033[43m"
+	ON_BLUE = "\033[44m"
+	ON_MAGENTA = "\033[45m"
+	ON_CYAN = "\033[46m"
+	ON_WHITE = "\033[47m"
 
-	def out(self, message, color, style=None, bg=None):
-		options = self.colors[color]
-		if style:
-			options += self.styles[style]
-		if bg:
-			options += self.backgroundColors[bg]
-
-		return options+message+self.reset
+	def out(self, message, *args):
+		style = ""
+		for s in args:
+			style += s
+		return style+message+self.RESET
